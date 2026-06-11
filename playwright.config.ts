16b839d9504b274d3aad5ab28f98164bb3cd1e62
tracy <8510537+tracychen@users.noreply.github.com>
2026-06-11 02:34:17 -0700

    Fix Playwright chromium viewport

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,10 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        ...devices["Desktop Chrome"],
+        viewport: { width: 1440, height: 1000 },
+      },
     },
   ],
 });
